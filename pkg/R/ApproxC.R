f319aa40470481c787fdc8835339863a7133b222
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2016-07-15 20:16:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hpcwld@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ApproxC.R
+++ pkg/R/ApproxC.R
@@ -1,11 +1,11 @@
+# This gives the constant C in the inequality lambda C/mu <1 of the cluster model stability condition
 ApproxC=function(s,p,depth=3){
 	A=cumsum(p[s:1])
 	B=p
 	C=0
 	for(i in 1:depth){
 	C=C+sum(B[1:(s-i+1)]*A[i:s])/i
-	B=convolve(B,rev(p),type="o")
+	B=convolve(B,rev(p),type="o")[1:s]
 	}
-	1/C
+	return(C)
 }
-#fApproxCold=function(s,p){ C=sum(cumsum(p[s:1])*p)+0.5*sum(convolve(p,rev(p),type="o")[1:(s-1)]*cumsum(p[s:1])[1:(s-1)]); 1/C   }
