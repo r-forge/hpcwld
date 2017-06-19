6f5732de79891b8008fca6064ad18f34b2693e6e
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2017-06-19 10:32:34 +0000

    modified EnergyWorkload
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hpcwld@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/EnergyWorkload.R
+++ pkg/R/EnergyWorkload.R
@@ -1,6 +1,6 @@
 EnergyWorkload = function(k1=1,k2=1,num.iter=10) # Function to compare Energy and Workload for various switching thresholds
 {
-r=c(0.9,1.5) # Speeds 1 and 2
+r=c(0.6,1.5) # Speeds 1 and 2
 e0=1 # Energy consumption at idle
 e=c(2,4) # Energy consumption at speeds 1 and 2
 
