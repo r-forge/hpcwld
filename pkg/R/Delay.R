b758fdaf4ef03b0b32df4afecc110eddeddc1264
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2018-02-08 11:40:12 +0000

    rewritten the recursion to speedup things
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hpcwld@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Delay.R
+++ pkg/R/Delay.R
@@ -1,16 +1,8 @@
-Delay=function(T,S,N,s) {
-n=length(T)
-workload=rep(0,s)
-Delay=rep(0,n)
-
-for(i in 1:n){
-	Delay[i]=workload[N[i]]
-	for(j in 1:N[i])
-		workload[j]=max(Delay[i]+S[i]-T[i],0)
-	if(N[i]<s)
-		for(j in (N[i]+1):s)
-			workload[j]=max(workload[j]-T[i],0)
-	workload=sort(workload)
-	}
+Delay=function(T,S,N,s,n=length(T),init.workload=rep(0,s)) {
+workload=init.workload
+Delay=vector("numeric",n)
+for(i in 1:n){ 
+  Delay[i]=workload[N[i]]
+  workload=sort(pmax((Delay[i]+S[i])*(1:s<=N[i])+workload*(1:s>N[i])-T[i], 0))}
 Delay
 }
