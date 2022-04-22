eb3186e23de360682b875b2d4890dbd5c1775aa0
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2022-04-22 12:57:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hpcwld@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Delay.R
+++
@@ -1,8 +0,0 @@
-Delay=function(T,S,N,s,n=length(T),init.workload=rep(0,s)) {
-workload=init.workload
-Delay=vector("numeric",n)
-for(i in 1:n){ 
-  Delay[i]=workload[N[i]]
-  workload=sort(pmax((Delay[i]+S[i])*(1:s<=N[i])+workload*(1:s>N[i])-T[i], 0))}
-Delay
-}
