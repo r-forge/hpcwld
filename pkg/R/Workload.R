d5980fa44351affbf44a7ccc96c0eb42e7eeb265
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2024-03-15 09:57:23 +0000

    Bugfix and version update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hpcwld@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Workload.R
+++ pkg/R/Workload.R
@@ -53,8 +53,8 @@ Wld <-
       else
         for(j in 1:N[i])
           workload[i+1,j]=max(workload[i,N[i]]+S[i,j]-T[i],0)
-        for(j in (N[i]+1):m)
-          if(j<=m && m>1)
+        if(N[i]<m)
+          for(j in (N[i]+1):m)
             workload[i+1,j]=max(workload[i,j]-T[i],0)
         workload[i+1,]=sort(workload[i+1,])
     }
